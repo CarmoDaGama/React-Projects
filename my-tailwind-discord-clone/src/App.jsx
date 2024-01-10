c497b4154a3cc26584a385dc16e80b777354ee4c
CARMO_DA_GAMA <carmodagama@gmail.com>
2024-01-10 12:56:15 +0100

    add ServerSidebar Component

--- my-tailwind-discord-clone/src/App.jsx
+++ my-tailwind-discord-clone/src/App.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import SideBar from "./components/SideBar";
+import ServerSideBar from "./components/ServerSideBar"
 
-function App() {
+function App() {  
+  //const [channelName] = useState("general");
   return (
     <div className="flex">
       <SideBar />
-
+      <div className="flex flex-col">
+        <ServerSideBar />
+      </div>
     </div>
   );
 }
