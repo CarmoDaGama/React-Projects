b493b73b3cc2dec6f83e4ab04a4b12d8142f6e22
CARMO_DA_GAMA <carmodagama@gmail.com>
2024-01-10 12:17:40 +0100

    add sidebar Component and implementatation of index.css

--- my-tailwind-discord-clone/src/App.js
+++
@@ -1,5 +0,0 @@
-function App() {
-  return <h1 className="text-3xl font-bold underline text-yellow-500">Hello world!</h1>;
-}
-
-export default App;
---
+++ my-tailwind-discord-clone/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import SideBar from "./components/SideBar";
+
+function App() {
+  return (
+    <div className="flex">
+      <SideBar />
+
+    </div>
+  );
+}
+
+export default App;
