428e79f6eac6fbd9e2ab8cfa250340fcc8812a50
CARMO_DA_GAMA <carmodagama@gmail.com>
2024-01-10 11:03:03 +0100

    add new package

--- my-tailwind-discord-clone/src/App.js
+++ my-tailwind-discord-clone/src/App.js
@@ -1,5 +1,5 @@
 function App() {
-  return <h1 className="text-3xl font-bold underline text-cyan-400">Hello world!</h1>;
+  return <h1 className="text-3xl font-bold underline text-yellow-500">Hello world!</h1>;
 }
 
 export default App;
